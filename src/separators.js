9107debcc938d497233555cb761e35173a774d4d
lyzready <1344114680@qq.com>
2016-05-17 05:52:57 -0700

    commit1

--- src/separators.js
+++ src/separators.js
@@ -1,7 +1,11 @@
 'use strict';
 
 function thousands_separators(num) {
-	return num;  
+	string re;
+  	while(num/=1000){
+		re=","+(String)((num-(int)num)*1000)+re;
+	return (string)(num*1000) +re;
+
 }
 
 module.exports = thousands_separators;
