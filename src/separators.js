dffd56de6fd18847ce5109e11d812052582ff928
lyzready <1344114680@qq.com>
2016-05-17 08:27:21 -0700

    commit8

--- src/separators.js
+++ src/separators.js
@@ -3,7 +3,7 @@
 function thousands_separators(num) {
 	var re="";
 	var n=num;
-  	while(n/=1000){
+  	while(n/=1000>1){
 		re=","+(String)(1000*(n-Math.floor(n)))+re;
 	}
 	return (String)(n*1000) +re;
