f239c159061fff4fda52b0515ca41bc313166d13
lyzready <1344114680@qq.com>
2016-05-18 04:14:34 -0700

    finished2

--- src/separators.js
+++ src/separators.js
@@ -3,7 +3,7 @@
 function thousands_separators(num) {
 	var n=num.toString().split('.');
 	var inte_spl=n[0].split('');
-	for(var i=inte_spl.length-4;i>0;i-=3){
+	for(var i=inte_spl.length-4;i>=0;i-=3){
 		inte_spl[i]+=',';
 	}
 	n[0]=inte_spl.join('');
