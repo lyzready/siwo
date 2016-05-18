ddabc70a6b6ff658840d632d656abe9aa2fbb343
lyzready <1344114680@qq.com>
2016-05-18 03:33:34 -0700

    error1

--- src/separators.js
+++ src/separators.js
@@ -3,12 +3,11 @@
 function thousands_separators(num) {
 	var re="";
 	var n=num;
-  	while(n/=1000>1){
-		re=","+(String)(1000*(n-Math.floor(n)))+re;
+ 	while((n/=1000)>1){
+		re=","+(1000*(n-Math.floor(n))).toString()+re;
 		n=Math.floor(n);
 	}
-	return (String)(n*1000) +re;
-
+	return (n*1000).toString() +re;
 }
 
 module.exports = thousands_separators;
