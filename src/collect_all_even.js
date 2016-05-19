cfebbb1c23b1e766fde833d729119b4bc96f306e
lyzready <1344114680@qq.com>
2016-05-19 04:13:29 -0700

    finished4

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -3,7 +3,7 @@ function collect_same_elements(collection_a, object_b) {
 	var re=[];
 	for(var i=0;i<collection_a.length;i++){
 		if(object_b.value.some(SameTo,collection_a[i].key))
-			re.splice(0,0,collection_a[i].key);
+			re.splice(re.length,0,collection_a[i].key);
 	}
 	return re;	
 	
