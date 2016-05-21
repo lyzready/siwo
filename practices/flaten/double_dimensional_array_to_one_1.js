1ce6243b062a82466e913d77407a0ac34c671fb6
lyzready <1344114680@qq.com>
2016-05-21 08:03:29 -0700

    finish1

---
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -0,0 +1,16 @@
+'use strict';
+
+function double_to_one(collection) {
+
+ var re=[];
+	for( var n1=0 ; n1< collection.length; n1++){
+		if(i[n1].length == undefined)
+			re.push(collection[n1]);
+		else
+			for(var n2 = 0;n2< collection[n1].length;n2++)
+				re.push(collection[n1][n2]);
+	}
+	return re;
+}
+
+module.exports = double_to_one;
---
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -0,0 +1,19 @@
+'use strict';
+
+function double_to_one(collection) {
+	var re=[];
+	for( var n1=0 ; n1< collection.length; n1++){
+		if(i[n1].length == undefined && !re.some(check,collection[n1]))
+			re.push(collection[n1]);
+		else
+			for(var n2 = 0;n2< collection[n1].length;n2++)
+				if(!re.some(check,collection[n1][n2]))
+					re.push(coection[n1][n2]);
+	}
+	return re;
+}
+function check(ii){
+ if(ii==this)
+	return 1;
+}
+module.exports = double_to_one;
