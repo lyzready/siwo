100a6745348b56aa477db71ac66ed12044a900ee
lyzready <1344114680@qq.com>
2016-05-24 08:10:15 -0700

    finish4

--- practices/flaten/double_dimensional_array_to_one_1.js
+++ practices/flaten/double_dimensional_array_to_one_1.js
@@ -1,15 +1,15 @@
 'use strict';
 
 function double_to_one(collection) {
-	var re=[];
+	var re=new Array();
 	for( var n1=0 ; n1< collection.length; n1++){
-		if(Array.isArray(re[n1]) == false)
+		if(Array.isArray(collection[n1]) == false)
 			re.push(collection[n1]);
 		else
 			for(var n2 = 0;n2< collection[n1].length;n2++)
 				re.push(collection[n1][n2]);
 	}
-	return [1,2,3,4];
+	return re;
 }
 
 module.exports = double_to_one;
--- practices/flaten/double_dimensional_array_to_one_2.js
+++ practices/flaten/double_dimensional_array_to_one_2.js
@@ -3,7 +3,7 @@
 function double_to_one(collection) {
 	var re=[];
 	for( var n1=0 ; n1< collection.length; n1++){
-		if(Array.isArray(re[n1]) == false && !re.some(check,collection[n1]))
+		if(Array.isArray(collection[n1]) == false && !re.some(check,collection[n1]))
 			re.push(collection[n1]);
 		else
 			for(var n2 = 0;n2< collection[n1].length;n2++)
