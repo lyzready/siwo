95347e399a7287e5d25690a423eb6fb9f2d9f626
lyzready <1344114680@qq.com>
2016-05-29 07:59:15 -0700

    finish

--- practices/section_1/practice_1/practice.js
+++ practices/section_1/practice_1/practice.js
@@ -3,7 +3,7 @@ function collect_same_elements(collection_a, collection_b) {
   for (var i = 0; i < collection_a; i++)
   {
 	if(collection_b.indexOf(collection_a[i])!=-1 &&
-		re.indexOf(collection_a[i] == -1)//排除已包含元素
+		re.indexOf(collection_a[i] == -1))//排除已包含元素
 	{re.push(collection_a[i]);}	
   }
   return re;
