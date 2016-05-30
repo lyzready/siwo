068b9b96b11c0c0e7e07adf2afc1128cc1edf38b
lyzready <1344114680@qq.com>
2016-05-30 08:02:23 -0700

    finish1

--- src/fibonacci_series.js
+++ src/fibonacci_series.js
@@ -2,13 +2,13 @@
 
 function fibonacci_series(n) {
 	var re =[0];
-	myfunction(re,n);
+	myfunction(re,n-1);
 	return re;
 }
 function myfunction(re,n){
 	if(n>2)
 		return re[n]=myfunction(n-1)+myfunction(n-2);
 	else
-		return re[1]=re[2]=1;
+		return re[n]=n;
 }
 module.exports = fibonacci_series;
